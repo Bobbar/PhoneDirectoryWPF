a930b2decacf1ec558203f84b3c45570ffd55ae1
Bobbar <Bobbar@gmail.com>
2018-07-19 14:19:29 -0400

    Deployment config.

--- PhoneDirectoryWPF/Properties/AssemblyInfo.cs
+++ PhoneDirectoryWPF/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PhoneDirectoryWPF")]
+[assembly: AssemblyTitle("Phone Directory")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PhoneDirectoryWPF")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright FCDD©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
